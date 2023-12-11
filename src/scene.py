53ac08af30ae69d8cf4a726b458864d46a530c8b
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2023-12-11 16:57:55 +0100

    add Camera and light source sheet

--- src/scene.py
+++ src/scene.py
@@ -1,16 +1,41 @@
+import bpy
+import src.utils.helper_methods as hm
+import imp
+imp.reload(hm)
+
 class Camera:
-    pass
+    def __init__(self, name='camera 1', pos = (0, 0, 2), rot = (0, 0, 0), size = (2, 2)):
+        scn = bpy.context.scene
+        cam = bpy.data.cameras.new(name)
+        cam.lens = 18
+
+        # create camera object
+        cam_obj = bpy.data.objects.new(name, cam)
+        cam_obj.location = pos
+        cam_obj.rotation_euler = rot
+        scn.collection.objects.link(cam_obj)
 
 
 class LightSource:
-    pass
+    def __init__(self, name='lightsource'):
+        bpy.ops.mesh.primitive_plane_add(size=2, location=(0, 0, -0.2))
 
+        # add shading
+        
+        
 
 class BioMedicalScene:
 
-    def __init__(self, light_source: LightSource, camera:Camera):
+    def __init__(self, light_source: LightSource, camera: Camera):
         self.light_source = light_source
         self.camera = camera
 
+    @staticmethod
+    def clear():
+        hm.delete_objects()
+    
+    def add_arangement(self):
+        pass
+    
     def render(self):
         pass
