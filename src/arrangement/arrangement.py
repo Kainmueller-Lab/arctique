3e74b572d8252642e6044811fe5a96052c48d7bf
chrzzl <christophantoine.karg@mdc-berlin.de>
2023-12-12 17:33:03 +0100

    Update arrangement.py

--- src/arrangement/arrangement.py
+++ src/arrangement/arrangement.py
@@ -3,6 +3,9 @@ from mathutils import Vector
 from src.objects.cells import Cell
 
 class CellArrangement:
+    # Class variable to keep track of the count
+    count = 0
+
     def __init__(self):
         pass
 
@@ -14,28 +17,32 @@ class CellArrangement:
             cell.add()
 
 class CellList(CellArrangement):
-    def __init__(self, cell_attributes, locations, name=None):
+    def __init__(self, cell_attributes, locations):
         self.cell_attributes = cell_attributes
         self.locations = locations
-        self.name = name
+        self.type = "LIST"
+        self.id = CellArrangement.count
+        CellArrangement.count += 1
         self.objects = []
 
     def generate_cells(self):
         for idx, location in enumerate(self.locations):
             # TODO: Make sure the ids are not just from the same range in different lists.
             # They need to be unique for each scene. - ck
-            cell = Cell(idx, self.cell_attributes, location, self.name)
+            cell = Cell(idx, location, self.id, self.type, self.cell_attributes)
             self.objects.append(cell)
         
 
 # TODO: Extend to different distribution types, e.g., Gaussian distribution, etc.
 class CellDistribution(CellArrangement):
-    def __init__(self, cell_attributes, num_cells, min_coords, max_coords, name=None):
+    def __init__(self, cell_attributes, num_cells, min_coords, max_coords):
         self.cell_attributes = cell_attributes
         self.num_cells = num_cells
         self.min_coords = min_coords
         self.max_coords = max_coords
-        self.name = name
+        self.type = "DIST"
+        self.id = CellArrangement.count
+        CellArrangement.count += 1
         self.objects = []
 
     def generate_cells(self):
@@ -46,5 +53,5 @@ class CellDistribution(CellArrangement):
                 random.uniform(self.min_coords.y, self.max_coords.y),
                 random.uniform(self.min_coords.z, self.max_coords.z)
             ])
-            cell = Cell(idx, self.cell_attributes, location, self.name)
+            cell = Cell(idx, location, self.id, self.type, self.cell_attributes)
             self.objects.append(cell)
