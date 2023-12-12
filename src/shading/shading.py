0c9b25f94252ea8bbdd5c73c367a51b94233fab9
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2023-12-12 14:47:17 +0100

    add preliminary tissue material

--- src/shading/shading.py
+++ src/shading/shading.py
@@ -6,10 +6,11 @@ class Material():
         for material in bpy.data.materials:
             bpy.data.materials.remove(material)
         self.light_source = self.add_light_source()
+        self.tissue_staining = self.add_tissue_staining()
 
-    def add_light_source(self, brightness=60):
+    def add_light_source(self, brightness=60, name='light_source'):
         # add new material and node tree
-        material = bpy.data.materials.new(name="light_source")
+        material = bpy.data.materials.new(name=name)
         material.use_nodes = True
         nodes = material.node_tree.nodes
 
@@ -27,5 +28,19 @@ class Material():
     def add_nuclei_staining(self, name):
         pass
 
-    def add_tissue_staining():
-        pass
+    def add_tissue_staining(self, transmission=0.929, name="tissue_staining", base_color=(0.62, 0.25, 0.65, 1.0)):
+        # add new material and node tree
+        material = bpy.data.materials.new(name=name)
+        material.use_nodes = True
+        nodes = material.node_tree.nodes
+
+        # add emission node
+        principled_bsdf = nodes.get("Principled BSDF")
+        principled_bsdf.inputs['Transmission'].default_value = transmission
+        principled_bsdf.inputs['Base Color'].default_value = base_color  # RGB color
+
+        # link nodes
+        material_output = material.node_tree.nodes.get('Material Output')
+        material.node_tree.links.new(principled_bsdf.outputs['BSDF'], material_output.inputs['Surface'])
+
+        return material
