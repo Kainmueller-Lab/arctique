69cc294a09aaf6999261bf14f5b0ce3f3738920e
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2023-12-12 12:16:19 +0100

    add light source shading

--- src/shading/shading.py
+++ src/shading/shading.py
@@ -2,10 +2,27 @@ import bpy
 
 class Material():
     def __init__(self):
-        pass
+        # delete all materials
+        for material in bpy.data.materials:
+            bpy.data.materials.remove(material)
+        self.light_source = self.add_light_source()
 
-    def add_lightsource(self):
-        pass
+    def add_light_source(self, brightness=60):
+        # add new material and node tree
+        material = bpy.data.materials.new(name="light_source")
+        material.use_nodes = True
+        nodes = material.node_tree.nodes
+
+        # add emission node
+        principled_bsdf = nodes.get("Principled BSDF")
+        principled_bsdf.inputs['Emission Strength'].default_value = 60.0
+        principled_bsdf.inputs['Emission'].default_value = (1.0, 1.0, 1.0, 1.0)  # RGB color
+
+        # link nodes
+        material_output = material.node_tree.nodes.get('Material Output')
+        material.node_tree.links.new(principled_bsdf.outputs['BSDF'], material_output.inputs['Surface'])
+
+        return material
 
     def add_nuclei_staining(self, name):
         pass
