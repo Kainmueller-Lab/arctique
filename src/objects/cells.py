d2a53ae85fa42d3dadcd8d00ab81f22c939f4c29
chrzzl <christophantoine.karg@mdc-berlin.de>
2023-12-20 16:37:03 +0100

    minor edits in cell.py

--- src/objects/cells.py
+++ src/objects/cells.py
@@ -1,5 +1,5 @@
 import bpy 
-#import bmesh
+import bmesh
 import random
 import math
 
@@ -29,7 +29,7 @@ class CellAttributeA(CellAttribute):
         self.max_bending_strength = max_bending_strength
 
 class CellAttributeB(CellAttribute):
-    def __init__(self, cell_type = "B", size = 0.07, scale = (2.3,1.3,1), deformation_strength = 0.02, attribute_name = "Cell Type B", max_bending_strength = 0.6):
+    def __init__(self, cell_type = "B", size = 0.07, scale = (2.3,1.3,1), deformation_strength = 0.05, attribute_name = "Cell Type B", max_bending_strength = 0.6):
         self.cell_type = cell_type
         self.size = size
         self.scale = scale
@@ -74,43 +74,125 @@ class Cell:
 
         # Apply two levels of subdivision surface
         modifier = self.cell_object.modifiers.new("Subsurface Modifier", "SUBSURF")
-        modifier.levels = 3    
+        modifier.levels = 2
+        bpy.ops.object.modifier_apply({"object": bpy.context.object}, modifier="Subsurface Modifier")
 
         # Deform the vertices proportionally in a random fashion
-        self.deform_mesh_old()
+        self.deform_mesh_old() # NOTE: This is just the random deformation method, the other needs moe care. - ck
         # Bend mesh along the z axis
         self.bend_mesh()
 
+
+    '''
+    translate random mesh vertices using proportional edit
+    i.e.: neighboring vertices are also translated proportionally
+    '''
     def deform_mesh(self):
         TRANSLATION_RANGE = 0.4
-        TRANSFORM_COUNT = 20
-        PROPORTIONAL_SIZE = 4        
-        # translate random mesh vertices using proportional edit
-        # i.e.: neighboring vertices are also translated proportionally
-        #bpy.ops.object.mode_set(mode='EDIT')
-        # bm = bmesh.from_edit_mesh(bpy.context.edit_object.data)
-        # # deselect all vertices
-        # for v in bm.verts:
-        #     v.select = False
-        # bm.verts.ensure_lookup_table() # NOTE: Necessary for accessing vertex list. - ck
-        # for i in range(TRANSFORM_COUNT):
+        TRANSFORM_COUNT = 3
+        PROPORTIONAL_SIZE = 1       
+        #translate random mesh vertices using proportional edit
+        #i.e.: neighboring vertices are also translated proportionally
+        #extract mesh
+        bpy.ops.object.mode_set(mode='EDIT')
+
+        context = bpy.context
+        bm = bmesh.from_edit_mesh(context.edit_object.data)
+        #deselect all vertices
+        for v in bm.verts:
+            v.select = False
+        bm.verts.ensure_lookup_table() # NOTE: Necessary for accessing vertex list
+        for i in range(TRANSFORM_COUNT):
+            transform = Vector([random.uniform(-1, 1),
+                        random.uniform(-1, 1),
+                        random.uniform(-1, 1)])*TRANSLATION_RANGE
+            v = bm.verts[random.randint(0, len(bm.verts) - 1)]
+            v.select = True
+            bpy.ops.transform.translate(value=transform, 
+                                    constraint_axis=(False, False, False),
+                                    orient_type='GLOBAL',
+                                    mirror=False, 
+                                    use_proportional_edit = True,
+                                    use_proportional_connected =True,
+                                    proportional_edit_falloff='SMOOTH',
+                                    proportional_size=PROPORTIONAL_SIZE)
+            v.select = False
+
+        # # get a reference to the active object
+        # mesh_obj = bpy.context.active_object
+
+        # bpy.context.view_layer.objects.active = mesh_obj
+
+        # ###### INITIALIZE BMESH ######
+        # # create a new bmesh
+        # bm = bmesh.new()
+        # # initialize the bmesh data using the mesh data
+        # bm.from_mesh(mesh_obj.data)
+
+        # ###### EDIT BMESH ######
+        # bm.verts.ensure_lookup_table() # NOTE: Necessary for accessing vertex list
+        # for _ in range(TRANSFORM_COUNT):
         #     transform = Vector([random.uniform(-1, 1),
         #                 random.uniform(-1, 1),
         #                 random.uniform(-1, 1)])*TRANSLATION_RANGE
         #     v = bm.verts[random.randint(0, len(bm.verts) - 1)]
-        #     v.select = True
-        #     bpy.ops.transform.translate(value=transform, 
-        #                             constraint_axis=(False, False, False),
-        #                             orient_type='GLOBAL',
-        #                             mirror=False, 
-        #                             use_proportional_edit = True,
-        #                             use_proportional_connected =True,
-        #                             proportional_edit_falloff='SMOOTH',
-        #                             proportional_size=PROPORTIONAL_SIZE)
-        # v.select = False
+        #     v.select_set(True)
+        #     print(bpy.context.selected_objects)
+        #     v.co += transform
+        #     # bpy.ops.transform.translate(value=transform, 
+        #     #                         constraint_axis=(False, False, False),
+        #     #                         orient_type='GLOBAL',
+        #     #                         mirror=False, 
+        #     #                         use_proportional_edit = True,
+        #     #                         use_proportional_connected =True,
+        #     #                         proportional_edit_falloff='SMOOTH',
+        #     #                         proportional_size=PROPORTIONAL_SIZE)
+        #     # print("Vertex translated")
+        #     #v.select_set(True)
 
-        # # Enter Object Mode
-        # bpy.ops.object.mode_set(mode='OBJECT')
+        # # bmesh.ops.bevel(
+        # #     bm,
+        # #     geom=bm.edges,
+        # #     offset=0.2,
+        # #     segments=4,
+        # #     affect="EDGES",
+        # #     profile=0.5,
+        # # )
+
+        # ###### UPDATE BMESH ######
+        # bm.normal_update()
+        # # writes the bmesh data into the mesh data
+        # bm.to_mesh(mesh_obj.data)
+        # # [Optional] update the mesh data (helps with redrawing the mesh in the viewport)
+        # mesh_obj.data.update()
+        # # clean up/free memory that was allocated for the bmesh
+        # bm.free()
+
+        # vertices = self.cell_object.data.vertices
+        # # Deselect all vertices
+        # for v in vertices:
+        #     pass
+        # #v.select = False
+        # # Transform random vertices
+        # for _ in range(TRANSFORM_COUNT):
+        #     for obj in bpy.context.selected_objects:
+        #         obj.select_set(False)   
+        #     transform = Vector([random.uniform(-1, 1),
+        #                 random.uniform(-1, 1),
+        #                 random.uniform(-1, 1)])*TRANSLATION_RANGE
+        #     random_index = random.randint(0, len(vertices) - 1)
+        #     v = vertices[random_index]
+        #     print_context_details()
+            #v.select = True
+            # bpy.ops.transform.translate(value=transform, 
+            #                         constraint_axis=(False, False, False),
+            #                         orient_type='LOCAL',
+            #                         use_proportional_edit = True,
+            #                         use_proportional_connected =True,
+            #                         proportional_edit_falloff='SMOOTH',
+            #                         proportional_size=PROPORTIONAL_SIZE)
+            #v.select = False
+        #bpy.ops.object.mode_set(mode='OBJECT')
 
     def bend_mesh(self):
         # Bend mesh along Z axis
