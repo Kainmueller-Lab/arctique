37e391ed52f5d517450dd99305a6d50c056e3014
Winklmayr <cwinklm@mdc-berlin.net>
2023-12-22 14:49:40 +0100

    add function to build instance mask

--- src/utils/plot_helpers.py
+++ src/utils/plot_helpers.py
@@ -45,3 +45,35 @@ def build_semantic_mask(source_folder, file_names, class_identifiers):
     """
 
     pass
+
+
+
+def build_instance_mask(source_folder, file_names): 
+    """
+    Combines individual cell masks to a semantic map of the full scene. 
+ 
+    Args:
+        source_folder (int): The folder where the masks can be found
+        file_names (list): List of names of the single masks
+        class_identifiers (list): LIst of strings identifying the different cell types as shown in file names  
+
+ 
+    Returns:
+        instance_mask: A png file where each cell object has a different pixel value
+    """
+
+    for file_idx, file_name in enumerate(file_names):   
+
+        mask_png = Image.open(Path(source_folder).joinpath(file_name))
+        mask_np = np.array(mask_png)
+
+        if file_idx == 0: 
+            # instance_mask is the same shape as the masks
+            instance_mask = np.zeros_like(mask_np) 
+    
+        instance_mask += mask_np*(file_idx/255.)
+
+        np.save(instance_mask, file=str(Path(source_folder).joinpath("instance_mask.npy")))
+        
+        instance_mask_png = Image.fromarray(instance_mask)
+        instance_mask.save(Path(source_folder).joinpath("instance_mask.png"))
