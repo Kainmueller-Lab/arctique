9931b9f4611f73ca42166c47afcd5421989b3862
Winklmayr <cwinklm@mdc-berlin.net>
2024-01-30 17:04:15 +0100

    changed name of argument

--- main.py
+++ main.py
@@ -70,24 +70,15 @@ my_scene.add_tissue(tissue=my_tissue.tissue)
 my_scene.cut_cells()
 my_scene.add_staining(material=my_materials.nuclei_staining)
 
-
-# Todo: 
-# remove all usage of + in path name generation
-# functionality for exporting depth mask
-
-
-# render scene
-#RENDER_PATH = 'C:/Users/cwinklm/Documents/Alpacathon/rendered_HE/renders/'
-RENDER_PATH = 'renders/'
+RENDER_PATH = 'C:/Users/cwinklm/Documents/Alpacathon/rendered_HE/renders2/'
 
 my_scene.render(filepath = RENDER_PATH,  # where to save renders
                scene = True, # if true scene is rendered
-               masks = True, # if true singel cell masks are rendered
+               single_masks = True, # if true singel cell masks are rendered
                semantic_mask = True, # if true semantic mask is generated
                instance_mask = True, # if true instance mask is generated
                depth_mask = True, # if true depth mask is generated
                obj3d = True, # if true scene is saved as 3d object
-               remove_single_masks = False, # if False single cell masks are deleted 
                output_shape = (500, 500), # dimensions of output
                max_samples = 10) # number of samples for rendering. Fewer samples will render more quickly. Default is 1024
 
