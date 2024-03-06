7051c1e467f70d04f6b664f72b98c0b6422f497a
chrzzl <christophantoine.karg@mdc-berlin.de>
2024-03-06 15:53:14 +0100

    make deformations better

--- src/utils/geometry.py
+++ src/utils/geometry.py
@@ -347,7 +347,7 @@ def random_point_in_bbox(bounding_box):
 def pos_value(x):
     return x if x>0 else 0
 
-def perturb_vertices(mesh, deform_strength):
+def perturb_vertices_old(mesh, deform_strength):
     VERTS_TO_MOVE = 8 # TODO: Take care of magical number
     PROPORTIONAL_SIZE = 0.5 # TODO: Necessary? If yes implement
     for _ in range(VERTS_TO_MOVE):
@@ -358,6 +358,18 @@ def perturb_vertices(mesh, deform_strength):
         for w in mesh.vertices:
             mu = deform_strength * pos_value(np.dot(direction, w.normal))
             w.co += direction * mu
+
+def perturb_vertices(mesh, scaled_deform_strength):
+    VERTS_TO_MOVE = 3 # TODO: Take care of magical number
+    PROPORTIONAL_SIZE = 0.4 # TODO: Necessary? If yes implement
+    # TODO: Test different deformations
+    for _ in range(VERTS_TO_MOVE):
+        #source_v = mesh.vertices[random.randint(0, len(mesh.vertices) - 1)]
+        #direction = source_v.normal
+        direction = Vector(random_unit_vector())
+        for w in mesh.vertices:
+            mu = scaled_deform_strength * pos_value(np.dot(direction, w.normal)-PROPORTIONAL_SIZE)
+            w.co -= direction * mu
     
 def rescale_obj(obj, size, scale):
     max_radius = max(np.linalg.norm(v.co) for v in obj.data.vertices)
@@ -367,12 +379,19 @@ def rescale_obj(obj, size, scale):
             
 def deform_mesh(obj, attribute):
     size = attribute.size
+    scale = attribute.scale
     deform_strength = attribute.deformation_strength
-    abs_deform_strength = size*deform_strength  
+    scaled_deform_strength = Vector(s*size*deform_strength for s in scale) 
     mesh = obj.data
-    perturb_vertices(mesh, abs_deform_strength)
-    # TODO: Rescale such that diameter of mesh lies in bounding ball
-    rescale_obj(obj, size, attribute.scale)
+    perturb_vertices(mesh, scaled_deform_strength)
+    # TODO: Old deform
+    # # TODO: Rescale such that diameter of mesh lies in bounding ball
+    # #rescale_obj(obj, size, scale)
+    # abs_deform_strength = size*deform_strength  
+    # mesh = obj.data
+    # perturb_vertices(mesh, abs_deform_strength)
+    # # TODO: Rescale such that diameter of mesh lies in bounding ball
+    # rescale_obj(obj, size, attribute.scale)
 
 
 def remove_top_and_bottom_faces(obj):
