94b683565e697600d912bbe33a569b84e55eca94
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2024-04-23 16:43:03 +0200

    split render and main

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 import bpy
 import sys
 import os
-import argparse
-import json
-import pathlib
-import bpycv
-import cv2
-import numpy as np
-import time
-import glob
 
-from os import listdir
-from os.path import isfile, join
-from tqdm import tqdm
 
 # IMPORT SOURCES
 dir = os.path.dirname(bpy.data.filepath)
 if not dir in sys.path:
-    sys.path.append(dir)
+    sys.path.append(dir )
 
 import src.arrangement.arrangement as arr 
 import src.objects.cells as cells
@@ -27,6 +16,7 @@ import src.scene as scene
 import src.utils as utils
 import src.utils.geometry as geom
 import src.utils.surface_filling as sf
+import src.utils.volume_filling as vf
 
 # this next part forces a reload in case you edit the source after you first start the blender session
 #import imp
@@ -39,6 +29,7 @@ imp.reload(scene)
 imp.reload(utils)
 imp.reload(geom)
 imp.reload(sf)
+imp.reload(vf)
 
 ###################  PARAMETER  #####################
 # args_camera = {'pos'} # no change just test
@@ -49,116 +40,77 @@ TISSUE_THICKNESS = 0.2
 TISSUE_SIZE = 2
 TISSUE_LOCATION = (0, 0, 0.5)
 TISSUE_PADDING = 0.5
-SURF_NUMBER = 80 
-FILLER_SCALE = 0.8 # Scale of the size of smaller filler nuclei w.r.t to the original nuclei size
-NUMBER = 80 
-RATIOS = [0.6, 0.2, 0.2]
-vol_scale = (1, 0.7, 1)
-surf_scale = (0.8, 0.5, 1)
+
 
 ###################  MAIN  METHOD  #####################
 # create the necessary objects
-
-def parse_dataset_args():
-    parser = argparse.ArgumentParser()
+scene.BioMedicalScene.clear()
     
-    parser.add_argument("--output_dir", type=str, default="J:/jannik/GitHub/rendered_HE/rendered", help="Set output folder")
-    parser.add_argument("--n_samples", type=int, default=1, help="Dataset size")
-    #other default value for --output_dir: "/Volumes/ag_kainmueller/vguarin/synthetic_HE" via internal VPN
-    
-    args = parser.parse_args()
-    
-    for arg in vars(args):
-        print(f"- {arg}: {getattr(args, arg)}")
-    return args
+# add microscope objects
+my_materials = shading.Material()
 
-def main():
-    args = parse_dataset_args() 
-    render_path = args.output_dir #render scene
-    print(render_path)
-    
-    #To continue creating samples from where we left off, use the following two lines of code, otherwise comment out 
-    dir = render_path + '/train_combined_masks/semantic'
+my_tissue = tissue.Tissue(my_materials.tissue_staining, thickness=TISSUE_THICKNESS, size=TISSUE_SIZE, location=TISSUE_LOCATION) # thickness and location of tissue should encapsulate min and max z-coordinates of cells 
+my_light_source = scene.LightSource(material=my_materials.light_source)
+my_camera = scene.Camera()
 
-    if not os.path.exists(dir):
-            os.makedirs(dir)
-    max_n_samples = len(glob.glob(f"{dir}/*.png")) 
-    # max_n_samples = len(os.listdir(dir)) if any(isfile(join(dir, i)) for i in listdir(dir)) else 0
-    # print(glob.glob("dir/*.png"), len(glob.glob("dir/*.png")))
+# create scene
+my_scene = scene.BioMedicalScene(my_light_source, my_camera)
 
+# TODO: Add documentation and docstrings
+# Define volume and surface objects
+# NOTE: In the end the volume and surface objects should come from the epithelial tissue macrostructure. - ck
+vol_scale = (1, 0.7, 1)
+surf_scale = (0.8, 0.5, 1)
+VOL_OBJ, SURF_OBJ = my_scene.add_dummy_objects(my_tissue, TISSUE_PADDING, vol_scale, surf_scale)
 
-    for i in tqdm(range(max_n_samples, max_n_samples + args.n_samples)):
-        scene.BioMedicalScene.clear()
-            
-        # add microscope objects
-        my_materials = shading.Material()
+# Create arrangements
+NUMBER = 100
+ATTRIBUTES = [cells.CellAttributeA(), cells.CellAttributeB(), cells.CellAttributeC()]
+RATIOS = [0.6, 0.2, 0.2]
+volume_fill = arr.VolumeFill(VOL_OBJ, NUMBER, ATTRIBUTES, RATIOS, strict_boundary=False)
+# NOTE: For some very weird reason you need to create the surface filling before the volume filling.
+# Otherwise the surface filling won't work and it won't even refine the mesh. :/ - ck
+# Add surface filling
+SURF_NUMBER = 40 # NOTE: The double amount of nuclei is added to the scene here due to additional filler nuclei. Might fix this if necessary. Prolly won't. Live with it and enjoy this humble life while you can. - ck
+SURF_ATTRIBUTE = cells.CellAttributeEpi()
+FILLER_SCALE = 0.8 # Scale of the size of smaller filler nuclei w.r.t to the original nuclei size
+surface_fill = arr.SurfaceFill(SURF_OBJ, SURF_NUMBER, SURF_ATTRIBUTE, FILLER_SCALE)
 
-        my_tissue = tissue.Tissue(my_materials.tissue_staining, thickness=TISSUE_THICKNESS, size=TISSUE_SIZE, location=TISSUE_LOCATION) # thickness and location of tissue should encapsulate min and max z-coordinates of cells 
-        my_light_source = scene.LightSource(material=my_materials.light_source)
-        my_camera = scene.Camera()
+# Add arrangements and rename nuclei with unique IDs
+my_scene.add_arrangement(volume_fill)
+my_scene.add_arrangement(surface_fill)
+my_scene.rename_nuclei()
 
-        # create scene
-        my_scene = scene.BioMedicalScene(my_light_source, my_camera)
-        # my_scene._clear_compositor()
+# Add tissue
+my_scene.add_tissue(tissue=my_tissue.tissue)
+my_scene.cut_cells()
+my_scene.add_staining(material=my_materials.nuclei_staining)
 
-        # TODO: Add documentation and docstrings
-        # Define volume and surface objects
-        # NOTE: In the end the volume and surface objects should come from the epithelial tissue macrostructure. - ck
-        VOL_OBJ, SURF_OBJ = utils.geometry.add_dummy_objects(my_tissue, TISSUE_PADDING, vol_scale, surf_scale)
+# Hide non cell objects
+my_scene.hide_non_cell_objects()
 
-        # NOTE: For some very weird reason you need to create the surface filling before the volume filling.
-        # Otherwise the surface filling won't work and it won't even refine the mesh. :/ - ck
-        # TODO: Fix that
-        # Add surface filling
-        SURF_ATTRIBUTE = cells.CellAttributeEpi()
-        surface_fill = arr.SurfaceFill(SURF_OBJ, SURF_NUMBER, SURF_ATTRIBUTE, FILLER_SCALE)
-        my_scene.add_arrangement(surface_fill)
+# render scene
+RENDER_PATH = 'C:/Users/cwinklm/Documents/Alpacathon/rendered_HE/renders2d_test/'
+#RENDER_PATH = 'renders/'
 
-        # Add volume filling
-        ATTRIBUTES = [cells.CellAttributeA(), cells.CellAttributeB(), cells.CellAttributeC()]
-        volume_fill = arr.VolumeFill(VOL_OBJ, NUMBER, ATTRIBUTES, RATIOS, strict_boundary=False)
-        my_scene.add_arrangement(volume_fill)
-
-        # Hide macro objects
-        VOL_OBJ.hide_viewport = True
-        VOL_OBJ.hide_render = True
-        SURF_OBJ.hide_viewport = True
-        SURF_OBJ.hide_render = True
-
-        # Add tissue
-        my_scene.add_tissue(tissue=my_tissue.tissue)
-        my_scene.cut_cells()
-        my_scene.add_staining(material=my_materials.nuclei_staining)
-    
-        my_scene.sample_name = i+1
-        
-        # attempt to use bpycv
-        # result = bpycv.render_data()
-        # # save result
-        # cv2.imwrite("/demo-rgb.jpg", result["image"][..., ::-1])  # transfer RGB image to opencv's BGR
-
-        my_scene.render(filepath = render_path,  # where to save renders
-                    scene = True, # if true scene is rendered
-                    single_masks = True, # if true single cell masks are rendered
-                    semantic_mask = True, # if true semantic mask is generated
-                    instance_mask = True, # if true instance mask is generated
-                    depth_mask = False, # if true depth mask is generated
-                    obj3d = False, # if true scene is saved as 3d object
-                    output_shape = (500, 500), # dimensions of output
-                    max_samples = 10) # number of samples for rendering. Fewer samples will render more quickly. Default is 1024
-
-        bpy.ops.wm.read_factory_settings(use_empty=True)
-
-        # my_scene.render3d(filepath = RENDER_PATH,  # where to save renders
-        #                scene = True, # if true scene is rendered
-        #                n_slices = 10,
-        #                semantic_mask = True, # if true semantic mask is generated
-        #                instance_mask = True, # if true instance mask is generated
-        #                depth_mask = True, # if true depth mask is generated
-        #                obj3d = True, # if true scene is saved as 3d object
-        #                output_shape = (500, 500), # dimensions of output
-        #                max_samples = 10) # number of samples for rendering. Fewer samples will render more quickly. Default is 1024
+my_scene.render(filepath = RENDER_PATH,  # where to save renders
+               scene = True, # if true scene is rendered
+               single_masks = True, # if true singel cell masks are rendered
+               semantic_mask = True, # if true semantic mask is generated
+               instance_mask = True, # if true instance mask is generated
+               depth_mask = True, # if true depth mask is generated
+               obj3d = True, # if true scene is saved as 3d object
+               output_shape = (500, 500), # dimensions of output
+               max_samples = 10) # number of samples for rendering. Fewer samples will render more quickly. Default is 1024
 
 
-if __name__ == "__main__":
-    main()
+
+# my_scene.render3d(filepath = RENDER_PATH,  # where to save renders
+#                scene = True, # if true scene is rendered
+#                n_slices = 10,
+#                semantic_mask = True, # if true semantic mask is generated
+#                instance_mask = True, # if true instance mask is generated
+#                depth_mask = True, # if true depth mask is generated
+#                obj3d = True, # if true scene is saved as 3d object
+#                output_shape = (500, 500), # dimensions of output
+#                max_samples = 10) # number of samples for rendering. Fewer samples will render more quickly. Default is 1024
