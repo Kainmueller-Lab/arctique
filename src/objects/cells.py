b84d81fc70521a2bc122f9ff4d61c8e6df8adba2
chrzzl <christoph.karg@mdc-berlin.de>
2024-05-23 23:15:27 +0200

    add mixing types

--- src/objects/cells.py
+++ src/objects/cells.py
@@ -11,12 +11,15 @@ from src.shading.shading import Material
 from src.utils.geometry import *
 
 class CellType(Enum):
-    PLA = 1
-    LYM = 2
-    EOS = 3
-    FIB = 4
-    EPI = 5
-    GOB = 6
+    PLA = 1,
+    LYM = 2,
+    EOS = 3,
+    FIB = 4,
+    EPI = 5,
+    GOB = 6,
+    MIX = 7
+
+TYPE_MIXING = 0.3
 
 
 class CellAttribute():
@@ -47,7 +50,7 @@ class PLA(CellAttribute):
                  cell_type = CellType.PLA,
                  size = 0.07,
                  scale = (1,0.6,0.5),
-                 deformation_strength = 0.5,
+                 deformation_strength = 0.7,
                  attribute_name = "Plasma Cell",
                  max_bending_strength = 0.2):
         super().__init__()
@@ -73,7 +76,7 @@ class LYM(CellAttribute):
                  cell_type = CellType.LYM,
                  size = 0.04,
                  scale = (1, 0.9, 0.8),
-                 deformation_strength = 0.5,
+                 deformation_strength = 0.7,
                  attribute_name = "Lymphocyte",
                  max_bending_strength = 0.2):
         super().__init__()
@@ -218,29 +221,42 @@ class GOB(CellAttribute):
 
 
 
-class MixAttribute:
+class MixAttribute(CellAttribute):
     def __init__(self, true_attribute: CellAttribute, mixing_attribute: CellAttribute, mix: float):
         '''
         Produces a cell attribute that is a combination of two cell attributes in terms of numerical values.
         Mix is the mixing strength of the true attribute and is a number between 0 and 1.
         Mix 0 produces the true attribute, mix 1 produces the mixing attribute.
         '''
+        super().__init__()
         self.true_attribute = true_attribute
         self.mixing_attribute = mixing_attribute
         self.mix = mix
 
-        self.cell_type = f"{true_attribute.cell_type}_Mix_{mix}_{mixing_attribute.cell_type}"
+        # TODO: Add lerp function
+        self.cell_type = CellType.MIX
         self.size = (1-mix)*true_attribute.size + mix*mixing_attribute.size
         self.scale = tuple((1-mix)*true_attribute.scale[i] + mix*mixing_attribute.scale[i] for i in range(3))
         self.deformation_strength = (1-mix)*true_attribute.deformation_strength + mix*mixing_attribute.deformation_strength
         self.attribute_name = f"{1-mix} {true_attribute.attribute_name} + {mix} {mixing_attribute.attribute_name}"
         self.max_bending_strength = (1-mix)*true_attribute.max_bending_strength + mix*mixing_attribute.max_bending_strength
+    
+    def add_nucleus_object(self, location, direction):
+            bpy.ops.mesh.primitive_ico_sphere_add(radius=self.size)
+            nucleus = bpy.context.active_object
+            deform_mesh(nucleus, self)
+            subdivide(nucleus, self.subdivision_levels)
+            set_orientation(nucleus, direction)
+            nucleus.location = location
+            nucleus.scale = self.scale
+            return nucleus
 
 cell_type_to_attribute = {
     CellType.PLA.name : PLA(),
-    CellType.FIB.name : FIB(),
-    CellType.EOS.name : EOS(),
     CellType.LYM.name : LYM(),
+    CellType.EOS.name : EOS(),
+    CellType.FIB.name : FIB(),
     CellType.EPI.name : EPI(),
-    CellType.GOB.name : GOB()
+    CellType.GOB.name : GOB(),
+    CellType.MIX.name : MixAttribute(PLA(), LYM(), TYPE_MIXING),
 }
