04926bc6e5b635bb12eef9554c1980f813395abf
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2024-06-01 16:05:23 +0200

    correct path

--- render_slurm.sh
+++ render_slurm.sh
@@ -9,7 +9,7 @@
 
 N_GPUS=1
 echo $SLURM_JOB_ID
-PYTHONPATH=/hkfs/work/workspace_haic/scratch/hgf_ysb1444-rendered_HE/rendered_HE python render.py "$@"
+PYTHONPATH=/hkfs/work/workspace_haic/scratch/hgf_lrx5378-darmstadt/rendered_HE python render.py "$@"
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
