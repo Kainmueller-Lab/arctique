f125535913f4e8b084b7a926c403ead9142b2a8e
jannikfranzen <93494199+jannikfranzen@users.noreply.github.com>
2024-06-26 16:29:27 +0200

    Update render.sh

--- render.sh
+++ render.sh
@@ -10,6 +10,7 @@
 ### $ -l gpu=1 -l cuda_name="Tesla-V100-SXM2-16GB"
 ### $ -l h=maxg03,h=maxg04,h=maxg05,h=maxg06,h=maxg07,h=maxg08,gpu=1
 ###$ -l cuda_name = "A40-PCIE-45G"
+### $ -pe mpi 4
 
 #$ -l gpu=1
 #$ -l m_mem_free=40G
@@ -19,7 +20,8 @@
 #$ -o out_log_$JOB_ID
 #$ -l h_rt=5:00:00
 #$ -A kainmueller
-#$ -pe mpi 1-10
+#$ -pe smp 1-10
+
 
 CUDA_VISIBLE_DEVICES=0
 
